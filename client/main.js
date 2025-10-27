d672bd8daa8f9aea33edb89ff0a924869fd62cd5
Yoosup Song <Youssupsong@gmail.com>
2025-10-27 15:07:33 -0400

    style: formatting

--- client/main.js
+++ client/main.js
@@ -1,31 +1,31 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow } = require("electron/main");
 
-const path = require('node:path')
+const path = require("node:path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  win.loadFile('index.html')
-}
+  win.loadFile("index.html");
+};
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
