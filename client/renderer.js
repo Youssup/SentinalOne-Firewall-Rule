cb36061949caff3a6a532c067a898e59b06ce573
Yoosup Song <Youssupsong@gmail.com>
2025-10-27 15:03:07 -0400

    feat: create default rule template *May need to be revised*

--- client/renderer.js
+++ client/renderer.js
@@ -6,3 +6,20 @@ const clearIpButton = document.getElementById("clear-ip-button");
 const loadFileButton = document.getElementById("load-file-button");
 const downloadButton = document.getElementById("download-button");
 const statusMessage = document.getElementById("status-message");
+
+const defaultRuleTemplate = {
+  data: {
+    name: "Blocking Rule Example",
+    action: "Block",
+    direction: "inbound",
+    protocol: "UDP",
+    status: "Enabled",
+    os_types: ["osx", "linux", "windows"],
+    remote_hosts: [{ type: "addresses", values: ["191.168.1.1"] }],
+    remote_port: [],
+    local_port: [],
+    application: [],
+    service: null,
+  },
+  filter: {},
+};
