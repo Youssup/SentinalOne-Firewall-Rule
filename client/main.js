8c05bf14689398181e86acef7a0492128242b8c7
Yoosup Song <Youssupsong@gmail.com>
2025-10-30 16:49:55 -0400

    feat: loading JSON firewall rules

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require("electron/main");
-
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const path = require("node:path");
+const fs = require("node:fs");
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -27,5 +27,30 @@ app.whenReady().then(() => {
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
+  }
+});
+
+// 
+ipcMain.handle("open-file", async () => {
+  // Open file window
+  const { canceled, filePaths } = await dialog.showOpenDialog({
+    title: "Open Firewall Rule",
+    filters: [{ name: "JSON Files", extensions: ["json"] }],
+    properties: ["openFile"],
+  });
+
+  //If the file was closed or no file selected send cancelled status back to renderer.js
+  if (canceled || filePaths.length === 0) {
+    return { status: "cancelled" };
+  }
+
+  // Get the first file path
+  const filePath = filePaths[0];
+  try {
+    // Read the file and send it back to renderer.js
+    const content = fs.readFileSync(filePath, "utf-8");
+    return { status: "success", content: content, path: filePath };
+  } catch (err) {
+    return { status: "error", message: err.message };
   }
 });
--- client/preload.js
+++ client/preload.js
@@ -1,7 +1,6 @@
-const { contextBridge } = require("electron/renderer");
+const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("versions", {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
+// Expose method to open file
+contextBridge.exposeInMainWorld("api", {
+  openFile: () => ipcRenderer.invoke("open-file"),
 });
